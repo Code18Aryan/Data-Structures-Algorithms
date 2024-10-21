60ae1994b041d1e2e964d969b4c5eeb816fa0cf7
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2024-10-21 22:21:59 +0530

    Add files via upload

---
+++ STACK/NSL.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+
+ public:
+
+    vector<int> nearestSmallestToLeft(vector<int>&nums){
+         
+          vector<int> ans;
+
+          int n = nums.size();
+
+          if( n == 0) return ans;
+
+          stack<int> st;
+
+          for(int i=0; i<n; i++){
+             
+              if(st.empty()) ans.push_back(-1);
+
+              else if(st.size() > 0 && st.top() < nums[i]) ans.push_back(st.top());
+
+              else if(st.size() > 0 && st.top() >= nums[i]){
+                 
+                  while(st.size() > 0 && st.top() >= nums[i]) st.pop();
+
+                  if(st.empty()) ans.push_back(-1);
+
+                  else ans.push_back(st.top());
+              }
+
+              st.push(nums[i]);
+          }
+
+          return ans;
+    }
+
+
+};
+
+
+
+
+int main(){
+
+
+    solution leetcode;
+
+    vector<int> nums = {1,3,5,2,1,4};
+
+   vector<int> ans =  leetcode.nearestSmallestToLeft(nums);
+
+   for(auto x : ans){
+     
+      cout<<x<<" ";
+   }
+
+    
+
+     
+      
+}
---
+++ STACK/NSR.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+
+ public:
+
+    vector<int> nearestSmallestToRight(vector<int>&nums){
+         
+          vector<int> ans;
+
+          int n = nums.size();
+
+          if( n == 0) return ans;
+
+          stack<int> st;
+
+          for(int i=n-1; i>=0; i--){
+             
+              if(st.empty()) ans.push_back(-1);
+
+              else if(st.size() > 0 && st.top() < nums[i]) ans.push_back(st.top());
+
+              else if(st.size() > 0 && st.top() >= nums[i]){
+                 
+                  while(st.size() > 0 && st.top() >= nums[i]) st.pop();
+
+                  if(st.empty()) ans.push_back(-1);
+
+                  else ans.push_back(st.top());
+              }
+
+              st.push(nums[i]);
+          }
+
+          reverse(ans.begin(),ans.end());
+
+          return ans;
+    }
+
+
+
+};
+
+
+
+
+int main(){
+
+
+    solution leetcode;
+
+    vector<int> nums = {1,3,5,2,1,4};
+
+   vector<int> ans =  leetcode.nearestSmallestToRight(nums);
+
+   for(auto x : ans){
+     
+      cout<<x<<" ";
+   }
+
+    
+
+     
+      
+}
