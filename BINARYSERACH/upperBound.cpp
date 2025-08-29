0962f4124870d5c43233079847eb58c64e7a6a3d
Raj Aryan <aryan900raj@gmail.com>
2025-08-30 01:44:44 +0530

    Added UpperBoundFunction

--- BINARYSERACH/upperBound.cpp
+++ BINARYSERACH/upperBound.cpp
@@ -5,14 +5,39 @@ using namespace std;
 // smallest index where the element at that index is greater than the target 
 
 
+int findUpperBound(vector<int>&nums, int target){
+
+    int low = 0;
+    int high = nums.size()-1;
+    int ans = -1;
+
+    while(low <= high){
+
+        int mid = low + (high-low)/2;
+
+        if(mid > target){
+
+            ans = mid;
+            high = mid-1;
+        }
+
+        else low = mid+1;
+    }
+
+    return ans;
+
+
+}
+
+
                  
 int main(){
 
     vector<int> nums = {1,3,4,4,5,6,10};
     int target = 7;
 
+    cout<<findUpperBound(nums,target);
 
-    
-            
- return 0;
+
+    return 0;
 }
