35c22052e706c7cc1366a7f09bedec15b450c61e
Raj Aryan <aryan900raj@gmail.com>
2025-08-30 01:40:39 +0530

    function called

---
+++ BINARYSERACH/upperBound.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Defination -> 
+// smallest index where the element at that index is greater than the target 
+
+
+                 
+int main(){
+
+    vector<int> nums = {1,3,4,4,5,6,10};
+    int target = 7;
+
+
+    
+            
+ return 0;
+}
